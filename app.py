eb5aaa3d750ddb1f2eb44065a83ee9f4991192a7
David Kuperman <kuperman@192-168-1-108.tpgi.com.au>
2023-03-11 11:07:43 +1100

    included functionality for shipper to post jobs

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from flask import (
 
 
 from models.users import create_shipper, create_carrier, get_shipper_by_email, get_carrier_by_email, update_carrier_profile, delete_carrier_profile, get_carrier_by_id
+from models.jobs import create_job
 from werkzeug.security import generate_password_hash, check_password_hash
 import re, database
 from cloudinary import CloudinaryImage
@@ -42,6 +43,7 @@ def login():
         if shipper and check_password_hash(shipper['password_hash'], password):
             user = shipper
             user_type = 'shipper'
+            session['user_id'] = user['id']
 
         #check if email and password match a carrier account
         carrier = get_carrier_by_email(email)       
@@ -66,6 +68,43 @@ def login():
 def logout():
     session.clear()
     return redirect('/')
+
+@app.route('/carrier_signup', methods = ['GET', 'POST'])
+def carrier():
+    if request.method == 'POST':
+
+        firstName = request.form.get('firstName')
+        lastName = request.form.get('lastName')
+        email = request.form.get('email')
+        phoneNumber =  request.form.get('phoneNumber')
+        companyName = request.form.get('companyName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        #uploading picture 
+        image = request.files.get('profile_pic')
+        uploaded_image = cloudinary.uploader.upload(image)
+        profile_pic = uploaded_image['url']
+        
+        if len(firstName) < 2:
+            flash('first Name error', category='error')
+        elif len(lastName) < 2:
+            flash('last Name error', category='error')
+        elif not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+            flash('Invalid email address', category='error')
+        elif len(phoneNumber) < 10:
+            flash('phone error', category='error')
+        elif len(companyName) < 3:
+            flash('company Name error', category='error')
+        elif password1 != password2:
+            flash('passwords do not match', category='error')
+        else:
+            password_hash = generate_password_hash(password1)
+            create_carrier(firstName,lastName, email, phoneNumber, companyName, password_hash, profile_pic)
+            flash('Account created!', category='success')
+            return redirect('/login')
+    
+    return render_template ('csignup.html')
+
 
 @app.route('/carrier_dashboard',methods = ['GET', 'POST'])
 def carrierDash():
@@ -163,44 +202,35 @@ def shipper():
             password_hash = generate_password_hash(password1)
             create_shipper(firstName,lastName, email, phoneNumber, companyName, password_hash)
             flash('Account created!', category='success')
-
+        return redirect('/login')
     return render_template ('ssignup.html')  
 
-@app.route('/carrier_signup', methods = ['GET', 'POST'])
-def carrier():
+@app.route('/shipper_job', methods=['GET', 'POST'])
+def shipper_job():
+    if 'user_firstName' not in session or session['user_type'] != 'shipper':
+        return redirect('/login')
     if request.method == 'POST':
+        shipper_id = session['user_id']
+        type = request.form.get('type')
+        weight = request.form.get('weight')
+        pick_up = request.form.get('pick_up')
+        drop_off = request.form.get('drop_off')
+        suburb = request.form.get('suburb')
+        state = request.form.get('state')
+        create_job(shipper_id, type, weight, pick_up, drop_off, suburb, state)
+        flash('Job added successfully', category='success')
+        return redirect('/shipper_dashboard')
+    return render_template('shippers_job_page.html',)
 
-        firstName = request.form.get('firstName')
-        lastName = request.form.get('lastName')
-        email = request.form.get('email')
-        phoneNumber =  request.form.get('phoneNumber')
-        companyName = request.form.get('companyName')
-        password1 = request.form.get('password1')
-        password2 = request.form.get('password2')
-        #uploading picture 
-        image = request.files.get('profile_pic')
-        uploaded_image = cloudinary.uploader.upload(image)
-        profile_pic = uploaded_image['url']
-        
-        if len(firstName) < 2:
-            flash('first Name error', category='error')
-        elif len(lastName) < 2:
-            flash('last Name error', category='error')
-        elif not re.match(r"[^@]+@[^@]+\.[^@]+", email):
-            flash('Invalid email address', category='error')
-        elif len(phoneNumber) < 10:
-            flash('phone error', category='error')
-        elif len(companyName) < 3:
-            flash('company Name error', category='error')
-        elif password1 != password2:
-            flash('passwords do not match', category='error')
-        else:
-            password_hash = generate_password_hash(password1)
-            create_carrier(firstName,lastName, email, phoneNumber, companyName, password_hash, profile_pic)
-            flash('Account created!', category='success')
-            return redirect('/login')
+@app.route('/shipper_job_list')
+def shipper_job_list():
+    if 'user_firstName' not in session or session['user_type'] != 'shipper':
+        return redirect('/login')
+    print(session)
+    shippers_jobs = database.select_many(f"SELECT * FROM jobs WHERE shipper_id={session['user_id']} ORDER BY id DESC")
+
+    return render_template('shipper_job.html', shippers_jobs = shippers_jobs)
     
-    return render_template ('csignup.html')
 
 if __name__ == "__main__":
     app.run
